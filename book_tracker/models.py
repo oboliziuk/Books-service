0004fd224c5d436faf2c2a8bb9ce3b0531da5d3d
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2025-03-27 19:17:01 +0200

    Format book tracker and Book service using Black

--- book_tracker/models.py
+++ book_tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    # COVER_CHOICES = models.TextChoices("HARD", "SOFT")
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hardcover"
+        SOFT = "SOFT", "Softcover"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=10, choices=CoverType.choices, blank=True)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = "books"
+
+    def __str__(self):
+        return f"{self.title} by {self.author} (ID: {self.id})"
---
+++ book_tracker/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from book_tracker.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    cover = serializers.ChoiceField(choices=Book.CoverType.choices)
+
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
